5fcaab2f77c7de15a26bd1dc96e1cf641b8b23a4
rose-tetteh <rose.tetteh@amalitech.com>
2024-12-02 19:04:13 +0000

    feat: add sort request

---
+++ src/main/java/com/rossie/websortingalgorithm/model/SortRequest.java
@@ -0,0 +1,11 @@
+package com.rossie.websortingalgorithm.model;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class SortRequest {
+    private int id;
+    private String algorithm;
+}

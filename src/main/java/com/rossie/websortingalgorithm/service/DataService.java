331010d95abfea3e3497b9895509ed11ee954aed
rose-tetteh <rose.tetteh@amalitech.com>
2024-12-03 09:10:10 +0000

    Remove commented code

--- src/main/java/com/rossie/websortingalgorithm/service/DataService.java
+++ src/main/java/com/rossie/websortingalgorithm/service/DataService.java
@@ -6,31 +6,39 @@ import org.springframework.stereotype.Service;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
+/**
+ * The type Data service.
+ */
 @Service
 public class DataService {
     private final Map<Integer, DataModel> dataStore = new HashMap<>();
     private final AtomicInteger idCounter = new AtomicInteger(1);
 
-//    public List<Integer> sort(String algorithm, List<Integer> data) {
-//        SortAlgorithm sorter = switch (algorithm.toLowerCase()) {
-//            case "quicksort" -> new QuickSort();
-//            case "heapsort" -> new HeapSort();
-//            case "mergesort" -> new MergeSort();
-//            case "radixsort" -> new RadixSort();
-//            case "bucketsort" -> new BucketSort();
-//            default -> throw new IllegalArgumentException("Unsupported sorting algorithm");
-//        };
-//        return sorter.sort(data);
-//    }
-
+    /**
+     * Get all data list.
+     *
+     * @return the list
+     */
     public List<DataModel> getAllData(){
         return new ArrayList<>(dataStore.values());
     }
 
+    /**
+     * Get data by id data model.
+     *
+     * @param id the id
+     * @return the data model
+     */
     public DataModel getDataById(int id){
         return dataStore.get(id);
     }
 
+    /**
+     * Get data by algorithm list.
+     *
+     * @param algorithm the algorithm
+     * @return the list
+     */
     public List<DataModel> getDataByAlgorithm(String algorithm){
         List<DataModel> result = new ArrayList<>();
         for (DataModel data : dataStore.values()){
@@ -41,6 +49,12 @@ public class DataService {
         return result;
     }
 
+    /**
+     * Create data data model.
+     *
+     * @param list the list
+     * @return the data model
+     */
     public DataModel createData(List<Integer> list){
         int id = idCounter.getAndIncrement();
         DataModel data = new DataModel();
@@ -51,6 +65,11 @@ public class DataService {
         return data;
     }
 
+    /**
+     * Delete data.
+     *
+     * @param id the id
+     */
     public void deleteData(int id){
         if (!dataStore.containsKey(id)){
             throw new NoSuchElementException("DataModel with ID "+ id + "not found");
@@ -58,6 +77,13 @@ public class DataService {
         dataStore.remove(id);
     }
 
+    /**
+     * Sort data model.
+     *
+     * @param id        the id
+     * @param algorithm the algorithm
+     * @return the data model
+     */
     public DataModel sort(int id, String algorithm) {
         DataModel data = dataStore.get(id);
         if (data == null)

89c54a5e470559fe9e2fe85b7e8d6e85eb91c2be
rose-tetteh <rose.tetteh@amalitech.com>
2024-12-03 09:35:15 +0000

    feat:add frontend feature

---
+++ src/main/java/com/rossie/websortingalgorithm/WebMvcConfig.java
@@ -0,0 +1,26 @@
+package com.rossie.websortingalgorithm;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        registry.viewResolver(resolver);
+    }
+
+    @Bean
+    public MappingJackson2HttpMessageConverter jsonMessageConverter() {
+        return new MappingJackson2HttpMessageConverter(); // Converts JSON to Java objects and vice versa
+    }
+}

be42b6a7f14cecac6867b4946295739d1ea53104
rose-tetteh <rose.tetteh@amalitech.com>
2024-12-04 16:08:33 +0000

    refactor:handle exceptions well

---
+++ src/main/java/com/rossie/websortingalgorithm/utils/InvalidInputException.java
@@ -0,0 +1,7 @@
+package com.rossie.websortingalgorithm.utils;
+
+public class InvalidInputException extends RuntimeException {
+    public InvalidInputException(String message) {
+        super(message);
+    }
+}
